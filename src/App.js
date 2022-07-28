24193b19f58fb72d88d3bb6cba9a15fd5e9f5ea0
Rafael Pires <supremo.lekos@gmail.com>
2022-07-28 22:37:50 +0100

    React Router Sidebar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,33 @@ import About from "./pages/AboutPage";
 import Products from "./pages/ProductsPage";
 import Contact from "./pages/ContactPage";
 import SingleProduct from "./pages/SingleProductPage";
+import Cart from "./pages/CartPage";
 import Default from "./pages/Default.js";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/SideBar";
+import Sidecart from "./components/SideCart";
+import Footer from "./components/Footer";
 
 function App() {
-  return <h1>hello from tech store</h1>;
+  return (
+    <>
+      {/* navbar, sidebar,cart, footer */}
+      <Navbar />
+      <Sidebar />
+      <Sidecart />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/products" exact element={<Products />} />
+        <Route path="/products/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Default />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
