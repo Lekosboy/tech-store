e6355f825d7b65a800d85e4a1bb679ae98738ab2
Rafael Pires <supremo.lekos@gmail.com>
2022-07-28 23:29:41 +0100

    Last one

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import { ProductConsumer } from "../context/Context";
 export default function HomePage() {
   return (
     <>
-      <h1>hello from home page</h1>
+      <ProductConsumer>
+        {(value) => {
+          console.log(value);
+          return <h1>Hello from HomePage</h1>;
+        }}
+      </ProductConsumer>
     </>
   );
 }
