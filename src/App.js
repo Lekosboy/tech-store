ed25b7c0de2b190c18e9ccb58321ca193f259f2f
Rafael Pires <supremo.lekos@gmail.com>
2022-07-28 22:04:56 +0100

    Folder structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Products from "./pages/ProductsPage";
+import Contact from "./pages/ContactPage";
+import SingleProduct from "./pages/SingleProductPage";
+import Default from "./pages/Default.js";
+
 function App() {
   return <h1>hello from tech store</h1>;
 }
